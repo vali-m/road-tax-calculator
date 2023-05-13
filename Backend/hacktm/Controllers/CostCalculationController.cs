823aa75c644b4e17b3de5561dc34913f23f217b6
Robert Solnoc <rsolnoc@valeo-it.com>
2023-05-13 22:09:30 +0300

    Cost Controller

--- Backend/hacktm/Controllers/CostCalculationController.cs
+++ Backend/hacktm/Controllers/CostCalculationController.cs
@@ -1,4 +1,5 @@
 ﻿using hacktm.Models;
+using hacktm.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,16 +9,85 @@ namespace hacktm.Controllers
     [ApiController]
     public class CostCalculationController : ControllerBase
     {
-        [HttpPost("distance")]
-        public CostResponseModel CalculateForRoute([FromBody] RouteModel model)
+        private readonly IStreetsRepository _streetsRepository;
+
+        public CostCalculationController(IStreetsRepository streetsRepository)
         {
-            return new CostResponseModel();
+            _streetsRepository = streetsRepository;
+        }
+
+        [HttpPost("distance")]
+        public ActionResult<CostResponseModel> CalculateForRoute([FromBody] RouteModel model)
+        {
+            CostResponseModel cost = new CostResponseModel();
+            double biggestDiffrenceOfTonage = 0;
+            
+            List<StreetModel> lowestTonageStreets = new List<StreetModel>();
+            foreach (var street in model.Streets)
+            {
+                var streetFromDb = _streetsRepository.FindByName(street.Name);
+                if (streetFromDb == null)
+                {
+                    continue;
+                }
+
+                if (streetFromDb.Accessible == false)
+                {
+                    return BadRequest($"The street with name {street.Name} is not accessible.");
+                }
+
+                if (streetFromDb.MaxWeight < model.WeightInTons)
+                {
+                    var diffrenceOfTonage = model.WeightInTons - streetFromDb.MaxWeight;
+                    if (diffrenceOfTonage > biggestDiffrenceOfTonage)
+                    {
+                        biggestDiffrenceOfTonage = diffrenceOfTonage;
+                        lowestTonageStreets.Clear();
+                        lowestTonageStreets.Add(street);
+                    }
+                    else if (diffrenceOfTonage == biggestDiffrenceOfTonage)
+                    {
+                        lowestTonageStreets.Add(street);
+                    }
+                }
+            }
+
+            cost.Cost = biggestDiffrenceOfTonage * 10 * model.RouteLengthInKm;
+            cost.HighestCostStreets = lowestTonageStreets;
+            cost.CostPerKm = model.RouteLengthInKm / cost.Cost;
+            return cost;
         }
 
         [HttpPost("day")]
-        public CostResponseModel CalculateForDay([FromBody] VehicleModel weight)
+        public ActionResult<CostResponseModel> CalculateForDay([FromBody] VehicleModel weight)
         {
-            return new CostResponseModel();
+            if(weight == null) 
+            {
+                return BadRequest("No weight provided");
+            }
+            CostResponseModel cost = new CostResponseModel();
+            if (weight.WeightInTons <= 3.5)
+            {
+                cost.Cost = 0;
+            }
+            else if(weight.WeightInTons > 3.5 && weight.WeightInTons <= 5)
+            {
+                cost.Cost = 50;
+            }
+            else if (weight.WeightInTons > 5 && weight.WeightInTons <= 7.5)
+            {
+                cost.Cost = 75;
+            }
+            else if (weight.WeightInTons > 7.5 && weight.WeightInTons <= 10)
+            {
+                cost.Cost = 100;
+            }
+            else
+            {
+                cost.Cost = 150;
+            }
+
+            return cost;
         }
     }
 }
