2f79cfa657030cebb29d964e6aac05d89d36f24a
Andrei Filip <afilip@valeo-it.com>
2023-05-13 22:29:50 +0300

    .

--- Backend/hacktm/Controllers/CostCalculationController.cs
+++ Backend/hacktm/Controllers/CostCalculationController.cs
@@ -1,5 +1,6 @@
 ﻿using hacktm.Entities;
 using hacktm.Models;
+﻿using hacktm.Models;
 using hacktm.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,8 +18,8 @@ namespace hacktm.Controllers
             _repository = repository;
         }
 
-        [HttpPost("distance")]
-        public CostResponseModel CalculateForRoute([FromBody] RouteModel model)
+        [HttpPost("distance1")]
+        public CostResponseModel CalculateForRoute1([FromBody] RouteModel model)
         {
             var streets = new List<Street>();
             foreach (var s in model.Streets)
@@ -35,14 +36,83 @@ namespace hacktm.Controllers
             {
                 Cost = streets.Count() * 10,
                 HighestCostStreets = streets.Select(s => new StreetModel { Name = s.Name }).ToList(),
-                CostPerKm = streets.Count() * 10 / model.RouteLengthInKm 
+                CostPerKm = streets.Count() * 10 / model.RouteLengthInKm
             };
+
+        }
+
+        [HttpPost("distance2")]
+        public ActionResult<CostResponseModel> CalculateForRoute2([FromBody] RouteModel model)
+        {
+            CostResponseModel cost = new CostResponseModel();
+            double biggestDiffrenceOfTonage = 0;
+            
+            List<StreetModel> lowestTonageStreets = new List<StreetModel>();
+            foreach (var street in model.Streets)
+            {
+                var streetFromDb = _repository.FindByName(street.Name);
+                if (streetFromDb == null)
+                {
+                    continue;
+                }
+
+                if (streetFromDb.Accessible == false)
+                {
+                    return BadRequest($"The street with name {street.Name} is not accessible.");
+                }
+
+                if (streetFromDb.MaxWeight < model.WeightInTons)
+                {
+                    var diffrenceOfTonage = model.WeightInTons - streetFromDb.MaxWeight;
+                    if (diffrenceOfTonage > biggestDiffrenceOfTonage)
+                    {
+                        biggestDiffrenceOfTonage = diffrenceOfTonage;
+                        lowestTonageStreets.Clear();
+                        lowestTonageStreets.Add(street);
+                    }
+                    else if (diffrenceOfTonage == biggestDiffrenceOfTonage)
+                    {
+                        lowestTonageStreets.Add(street);
+                    }
+                }
+            }
+
+            cost.Cost = biggestDiffrenceOfTonage * 10 * model.RouteLengthInKm;
+            cost.HighestCostStreets = lowestTonageStreets;
+            cost.CostPerKm = model.RouteLengthInKm / cost.Cost;
+            return cost;
         }
 
         [HttpPost("day")]
-        public CostResponseModel CalculateForDay([FromBody] VehicleModel weight)
+        public ActionResult<CostResponseModel> CalculateForDay([FromBody] VehicleModel weight)
         {
-            return new CostResponseModel();
+            if(weight == null) 
+            {
+                return BadRequest("No weight provided");
+            }
+            CostResponseModel cost = new CostResponseModel();
+            if (weight.WeightInTons <= 3.5)
+            {
+                cost.Cost = 0;
+            }
+            else if(weight.WeightInTons > 3.5 && weight.WeightInTons <= 5)
+            {
+                cost.Cost = 50;
+            }
+            else if (weight.WeightInTons > 5 && weight.WeightInTons <= 7.5)
+            {
+                cost.Cost = 75;
+            }
+            else if (weight.WeightInTons > 7.5 && weight.WeightInTons <= 10)
+            {
+                cost.Cost = 100;
+            }
+            else
+            {
+                cost.Cost = 150;
+            }
+
+            return cost;
         }
     }
 }
--- Backend/hacktm/Repositories/StreetsRepository.cs
+++ Backend/hacktm/Repositories/StreetsRepository.cs
@@ -14,26 +14,6 @@ namespace hacktm.Repositories
         public Street FindByName(string name)
         {
             return base.GetQuery().Where(s => this.Compare(s.Name, name)).FirstOrDefault();
-            //var simPairs = new List<Tuple<Street, double>>();
-            //foreach(var s in query)
-            //{
-            //    var similarity = this.Compare(s.Name, name);
-            //    if (similarity > 0.7)
-            //        simPairs.Add(new Tuple<Street, double>(s, similarity));
-            //}
-
-            //if (simPairs.Count == 0)
-            //    return null;
-
-            //Tuple<Street, double> result = simPairs[0];
-
-            //foreach (var p in simPairs)
-            //{
-            //    if(p.Item2 > result.Item2)
-            //        result = p;
-            //}
-
-            //return result.Item1;
         }
 
         private bool Compare(string a, string b)
