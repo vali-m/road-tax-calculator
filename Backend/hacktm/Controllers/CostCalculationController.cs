517ee00416b600e17eedf34acde15020f09d9b6b
Andrei Filip <afilip@valeo-it.com>
2023-05-13 18:32:50 +0300

    .

---
+++ Backend/hacktm/Controllers/CostCalculationController.cs
@@ -0,0 +1,23 @@
+﻿using hacktm.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hacktm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CostCalculationController : ControllerBase
+    {
+        [HttpPost("distance")]
+        public CostResponseModel CalculateForRoute([FromBody] RouteModel model)
+        {
+            return new CostResponseModel();
+        }
+
+        [HttpPost("day")]
+        public CostResponseModel CalculateForDay([FromBody] VehicleModel weight)
+        {
+            return new CostResponseModel();
+        }
+    }
+}
---
+++ Backend/hacktm/Models/CostResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace hacktm.Models
+{
+    public class CostResponseModel
+    {
+        public double Cost { get; set; }
+        public double CostPerKm { get; set; }
+
+        public List<StreetModel> HighestCostStreets { get; set; }
+    }
+}
